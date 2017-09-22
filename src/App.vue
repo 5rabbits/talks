60b66618d63de788ac79ef86a7d9ad07a8193aab
vladzur <vladzur@gmail.com>
2017-09-22 12:56:35 -0300

    First commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <top-menu/>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import TopMenu from '@/components/TopMenu'
+export default {
+  name: 'app',
+  components: { TopMenu }
+}
+</script>
+
+<style lang="scss">
+  @import "./assets/sass/app.scss";
+</style>
---
+++ src/components/TopMenu.vue
@@ -0,0 +1,37 @@
+<template>
+  <nav class="navbar">
+    <div class="navbar-brand">
+      <router-link class="navbar-item" to="/" exact>
+        5rabbits
+      </router-link>
+      <div class="navbar-burger burger" @click="toggleMenu" :class="{'is-active': navmenu}">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </div>
+    <div class="navbar-menu" :class="{'is-active': navmenu}">
+      <div class="navbar-start">
+        <router-link class="navbar-item" to="/random" exact>Random!</router-link>
+      </div>
+      <div class="navbar-end">
+        <router-link class="navbar-item" to="/login" exact>Login</router-link>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      navmenu: false
+    }
+  },
+  methods: {
+    toggleMenu () {
+      this.navmenu = !this.navmenu
+    }
+  }
+}
+</script>
