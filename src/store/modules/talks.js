ed840750a367447a0cd746f7e2412581d7a58ccf
vladzur <vladzur@gmail.com>
2017-09-22 18:40:16 -0300

    Talks store & firebase libs

---
+++ src/store/modules/talks.js
@@ -0,0 +1,36 @@
+// import * as types from '../mutation-types'
+
+// initial state
+const state = {
+  talk: {
+    id: null,
+    title: '',
+    description: '',
+    url: '',
+    date: null,
+    owner: null,
+    rating: []
+  }
+}
+
+// getters
+const getters = {
+
+}
+
+// actions
+const actions = {
+
+}
+
+// mutations
+const mutations = {
+
+}
+
+export default {
+  state,
+  getters,
+  actions,
+  mutations
+}
